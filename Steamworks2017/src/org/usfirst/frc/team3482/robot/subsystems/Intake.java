a5a42647b85e0cf53c55ed94cc3ceec562650353
team3482 <team3482@team3482>
2017-01-12 16:46:08 -0800

    push test branch

---
+++ Steamworks2017/src/org/usfirst/frc/team3482/robot/subsystems/Intake.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team3482.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Intake extends Subsystem {
+
+	public Intake() {	
+		
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
