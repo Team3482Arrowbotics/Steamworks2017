b097bef8cb3cf2610e97d983219633ac511ec67a
JennaCazalet <32112973+JennaCazalet@users.noreply.github.com>
2017-09-19 15:49:58 -0700

    Update Shootgun.java

--- Steamworks2017/src/org/usfirst/frc/team3482/robot/commands/Shootgun.java
+++ Steamworks2017/src/org/usfirst/frc/team3482/robot/commands/Shootgun.java
@@ -1,1 +1,27 @@
 public class Shootgun extends Command {
+  public Shootgun() {
+  }
+
+
+  protected void initialize() {
+  }
+  protected void execute() {
+    Robot.chassis.prepareShoot(shootButton);
+  }
+  protected boolean isFinished(){
+      return false;
+  }
+  protected void end(){
+    Robot.chassis.stopPrepareShoot();
+  }
+  protected void interrupted(){
+  }
+  
+
+
+
+
+
+
+}
+
